e3ad36f05c40b223ae1494e0e61ddfd5d5e9afec
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-08-03 01:09:46 +0200

    fix: changelog render

---
+++ public/CHANGELOG.md
@@ -0,0 +1,10 @@
+# 0.1
+- **Terminal**: Added Panes game
+### 0.1.1
+- **Terminal**: Register on mouse down instead of a full click
+# 0.2
+- **Terminal**: Added Numbers game
+### 0.2.1
+- **Terminal**: Add number solver for stupid people
+# ~~0.3~~ not yet
+- ~~**Terminal**: Added Same Color game~~
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,18 +7,4 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   ngxMarkdownVersion = '18.0.0'
-
-  markdown = `
-# 0.1
-- **Terminal**: Added Panes game
-### 0.1.1
-- **Terminal**: Register on mouse down instead of a full click
-# 0.2
-- **Terminal**: Added Numbers game
-### 0.2.1
-- **Terminal**: Add number solver for stupid people
-# ~~0.3~~ not yet
-- ~~**Terminal**: Added Same Color game~~
-`
-
 }
