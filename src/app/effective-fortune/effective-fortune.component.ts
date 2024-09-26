e1ac868bb34b79a3de8d07bb65bcee8b7222ffb2
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-09-26 18:43:56 +0200

    style: navbar redesign 2

---
+++ src/app/effective-fortune/effective-fortune.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-effective-fortune',
+  templateUrl: './effective-fortune.component.html',
+  styleUrl: './effective-fortune.component.scss'
+})
+export class EffectiveFortuneComponent {
+
+}
