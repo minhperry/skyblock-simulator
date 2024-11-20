63df10621354d8f48456463f793fc19903a76dbd
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-11-20 23:55:34 +0100

    hydrate client

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-
-
-import { AppComponent } from './app.component';
+import {provideClientHydration} from "@angular/platform-browser";
 
 @NgModule({
   imports: [
     ServerModule,
 ],
-  bootstrap: [AppComponent],
+  bootstrap: [],
+  providers: [
+    provideClientHydration()
+  ]
 })
 export class AppServerModule {}
