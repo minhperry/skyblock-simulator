e1fb18133fec922d5aaac21b20c8918a7e263a81
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-11-24 00:14:41 +0100

    basic rendering

--- src/app/hotm/hotm.component.ts
+++ src/app/hotm/hotm.component.ts
@@ -1,8 +1,10 @@
 import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
-import {InitialHotmTree, TreeNode} from "../../interfaces/constantData";
+import {AbilityState, InitialHotmTree, PerkState, TreeNode} from "../../interfaces/hotmData";
 import {NgClass} from "@angular/common";
 
 import {Nullable} from "../../interfaces/types";
+import {autoToHTML as parse} from "@sfirew/minecraft-motd-parser";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-hotm',
@@ -14,16 +16,17 @@ import {Nullable} from "../../interfaces/types";
 })
 export class HotmComponent implements OnInit {
   grid: Nullable<TreeNode>[][] = [];
+  selected: Nullable<TreeNode> = null;
 
   constructor(
     // private hotm: HotmBackendService
     @Inject(PLATFORM_ID) private platform: Object,
+    private san: DomSanitizer
   ) {
   }
 
   ngOnInit() {
     this.initializeGrid()
-    console.log(this.grid)
   }
 
   private initializeGrid() {
@@ -42,15 +45,36 @@ export class HotmComponent implements OnInit {
     }
   }
 
+  onCellClick(x: number, y: number) {
+    console.log(x, y)
+    this.selected = this.grid[x][y];
+  }
+
   protected getStateClass(node: Nullable<TreeNode>) {
-    return (node as TreeNode).state.state
+    return this.asTreeNode(node).state.state
   }
 
   protected asTreeNode(node: Nullable<TreeNode>): TreeNode {
     return node as TreeNode;
   }
 
-  protected isNotNull(node: Nullable<TreeNode>) {
+  protected isNotNull(node: Nullable<TreeNode>): boolean {
     return node !== null
+  }
+
+  protected isLevelable(node: Nullable<TreeNode>): boolean {
+    return !!node?.perk.maxLevel;
+  }
+
+  protected isAbility(node: Nullable<TreeNode>): boolean {
+    return node ? this.stateIsAbility(node.state.state) : false;
+  }
+
+  private stateIsAbility(state: AbilityState | PerkState): boolean {
+    return Object.values(AbilityState).includes(state as AbilityState)
+  }
+
+  protected parse(content: string | object): string {
+    return this.san.bypassSecurityTrustHtml(parse(content)) as string
   }
 }
