0800769fe5ab83a208c6080e8e83d769b6a766e8
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-13 22:45:27 +0200

    add swagger api docs 2

--- server/hypixelApi/swagger/swagger.ts
+++ server/hypixelApi/swagger/swagger.ts
@@ -1,1 +1,13 @@
 import swaggerJSDoc from 'swagger-jsdoc';
+
+export const swaggerSpec = swaggerJSDoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hypixel API Proxy',
+      version: '1.0.0',
+      description: 'A proxy for the Hypixel API, specifically for this website.',
+    }
+  },
+  apis: ['./server/hypixelApi/**/*.ts'],
+})
