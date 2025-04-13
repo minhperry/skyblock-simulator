1b7774bbdeb037d1ec24f802d81ee00eb762901d
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-13 22:01:58 +0200

    new error class

--- server/commons/error.ts
+++ server/commons/error.ts
@@ -1,48 +1,24 @@
-import express from "express";
-
-export class RequestError<T = unknown> {
-  private serverResponse: express.Response;
-
-  constructor(res: express.Response) {
-    this.serverResponse = res;
-  }
-
-  error(status: number, payload: T) {
-    if (status <= 499 && status >= 400) {
-      this.serverResponse.status(status).json(payload);
-    } else {
-      throw new Error('Invalid error status code!');
-    }
+export class MojangNotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'MojangNotFoundError';
+    Error.captureStackTrace(this, this.constructor)
   }
 }
 
-export const ErrorPayload = {
-  MALFORMED_NAME: {
-    id: 'MALFORMED_NAME',
-    error: 'Malformed name with invalid characters!'
-  },
-  NAME_TOO_LONG: {
-    id: 'NAME_TOO_LONG',
-    error: 'Name is too long!'
-  },
-  PLAYER_NOT_FOUND: {
-    id: 'PLAYER_NOT_FOUND',
-    error: 'Player not found!'
-  },
-  INVALID_PROFILE: {
-    id: 'INVALID_PROFILE',
-    error: 'Invalid profile!'
-  },
-  PROFILE_NOT_FOUND: {
-    id: 'PROFILE_NOT_FOUND',
-    error: 'Profile not found!'
-  },
-  ROUTE_NOT_FOUND: {
-    id: 'ROUTE_NOT_FOUND',
-    error: 'Route not found!'
-  },
-  RATE_LIMITED: {
-    id: 'RATE_LIMITED',
-    error: 'Too many requests, please try again later!'
+// This is a custom error class for Zod validation errors
+export class ZodValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'ZodValidationError';
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+export class DatabaseReadError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'DatabaseReadError';
+    Error.captureStackTrace(this, this.constructor)
   }
 }
