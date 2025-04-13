4c49b121b3e096d361112cb083d29a62e60af6bd
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-13 22:06:04 +0200

    add appwrite service

--- server/hypixelApi/appwrite/appwrite.service.ts
+++ server/hypixelApi/appwrite/appwrite.service.ts
@@ -1,14 +1,15 @@
 import {Player} from '../api/player/player.model';
-import {Client, Databases, Query} from 'appwrite';
+import {AppwriteException, Client, Databases, Query} from 'node-appwrite';
+import {DatabaseReadError} from '../../commons/error';
 
 // Appwrite client
 const $client = new Client();
 $client
+  .setKey(process.env['APPWRITE_DB_KEY']!)
   .setProject('67face2d002b747386e4')
-  .setEndpoint('https://cloud.appwrite.io/v1')
 
 // DB stuffs
-const $dbId = 'hypixel'
+const $dbId = 'hypixel-db'
 const DB = new Databases($client)
 
 export async function savePlayer(player: Player): Promise<void> {
@@ -25,8 +26,16 @@ export async function savePlayer(player: Player): Promise<void> {
   }
 }
 
-export async function getPlayerByName(playerName: string) {//: Promise<Player | null> {
+/**
+ * Get a player by name from Appwrite Database.
+ * @param playerName the name of the player to fetch
+ * @returns {`null`} if the player is not found, else returns the player data
+ * @throws {DatabaseReadError} if there is an error reading from the database
+ */
+export async function getPlayerByNameFromDB(playerName: string): Promise<Player | null> {
   try {
+    console.log(`Getting player ${playerName} from DB...`)
+
     // Query the database for the player by username
     const resp = await DB.listDocuments(
       $dbId,
@@ -40,11 +49,17 @@ export async function getPlayerByName(playerName: string) {//: Promise<Player | 
     if (resp.documents.length === 0) {
       return null
     }
-    console.log(resp.documents)
-    return null
+
     // If found, return the player data
-  } catch (e) {
-    console.log('Error getting player from database: ', e)
-    return null
+    const playerDoc = resp.documents[0] as unknown as Player;
+    return {
+      uuid: playerDoc.uuid,
+      username: playerDoc.username
+    }
+  } catch (e: unknown) {
+    if (e instanceof AppwriteException) {
+      console.error('Appwrite error:', e.message)
+    }
+    throw new DatabaseReadError('Error reading from Appwrite database')
   }
 }
