3883a217790acbb3f5e7f507fa57f5722f64a3a6
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-16 18:16:17 +0200

    wrap it as a class

--- server/hypixelApi/services/localdb.service.ts
+++ server/hypixelApi/services/localdb.service.ts
@@ -2,62 +2,72 @@ import {LocalPlayerDAO, Player} from '../api/player/player.model';
 import {ItemNotFoundError} from '../utils/error';
 import {getLogger} from '../utils/logger';
 import {JSONFilePreset} from 'lowdb/node';
-
-const logger = getLogger('services/localdb.service')
-
-const defaultData: { players: LocalPlayerDAO[] } = {players: []}
-const DB = await JSONFilePreset('players.json', defaultData)
+import {Low} from 'lowdb';
 
 /**
- * Saves a player to local db.
- * @param player the player to save
+ * This is a class that manages a local database for storing player data.
  */
-export async function savePlayer(player: Player): Promise<void> {
-  try {
+export class LocalDatabaseService {
+  private logger = getLogger('services/localdb.service')
+  private DB: Low<{ players: LocalPlayerDAO[] }>
+
+  constructor() {
+    this.defineDatabase().then(
+      lw => this.DB = lw
+    );
+    this.logger.level = 'debug'
+  }
+
+  private async defineDatabase() {
+    const defaultData: { players: LocalPlayerDAO[] } = {players: []}
+    return await JSONFilePreset('players.json', defaultData)
+  }
+
+  /**
+   * Saves a player to local db.
+   * @param player the player to save
+   */
+  savePlayer(player: Player) {
     logger.log(`Saving player ${player.username} to DB...`)
     const playerDAO = player.asLDAO()
     DB.data.players.push(playerDAO)
-  } catch (e) {
-    logger.error('Error saving player:', e)
-  }
-}
-
-/**
- * Get a player by name from Local Database.
- * @param playerName the name of the player to fetch
- * @throws {ItemNotFoundError} if the player is not found in the database
- */
-export function getPlayerByNameFromDB(playerName: string): Player {
-  logger.log(`Getting player ${playerName} from DB...`)
-  const {players} = DB.data
-
-  // Query the database for the player by username
-  const foundPlayer = players.find((pDAO) => pDAO.username === playerName)
-
-  if (!foundPlayer) {
-    logger.log(`Player ${playerName} not found in DB`)
-    throw new ItemNotFoundError(`with name ${playerName}`)
   }
 
-  return foundPlayer.asPlayer()
-}
+  /**
+   * Get a player by name from local database.
+   * @param playerName the name of the player to fetch
+   */
+  getPlayerByName(playerName: string): Player {
+    logger.log(`Getting player ${playerName} from DB...`)
+    const {players} = DB.data
 
-/**
- * Get a player by UUID from Database.
- * @param uuid the UUID of the player to fetch
- * @throws {ItemNotFoundError} if the uuid is not found in the database
- */
-export function getPlayerByUuidFromDB(uuid: string): Player {
-  logger.info(`Getting player with UUID ${uuid} from DB...`)
-  const {players} = DB.data
+    // Query the database for the player by username
+    const foundPlayer = players.find((pDAO) => pDAO.username === playerName)
 
-  // Query the database for the player by UUID
-  const foundPlayer = players.find((pDAO) => pDAO.uuid === uuid)
+    if (!foundPlayer) {
+      logger.log(`Player ${playerName} not found in DB`)
+      throw new ItemNotFoundError(`with name ${playerName}`)
+    }
 
-  if (!foundPlayer) {
-    logger.log(`Player with UUID ${uuid} not found in DB`)
-    throw new ItemNotFoundError(`with UUID ${uuid}`)
+    return foundPlayer.asPlayer()
   }
 
-  return foundPlayer.asPlayer()
+  /**
+   * Get a player by UUID from local database.
+   * @param uuid the UUID of the player to fetch
+   */
+  getPlayerByUuid(uuid: string): Player {
+    logger.info(`Getting player with UUID ${uuid} from DB...`)
+    const {players} = DB.data
+
+    // Query the database for the player by UUID
+    const foundPlayer = players.find((pDAO) => pDAO.uuid === uuid)
+
+    if (!foundPlayer) {
+      logger.log(`Player with UUID ${uuid} not found in DB`)
+      throw new ItemNotFoundError(`with UUID ${uuid}`)
+    }
+
+    return foundPlayer.asPlayer()
+  }
 }
