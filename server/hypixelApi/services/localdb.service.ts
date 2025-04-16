6614297c0b2387805d95e510d5b7ccdbb254b7f8
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-16 18:27:56 +0200

    export an instance

--- server/hypixelApi/services/localdb.service.ts
+++ server/hypixelApi/services/localdb.service.ts
@@ -7,7 +7,7 @@ import {Low} from 'lowdb';
 /**
  * This is a class that manages a local database for storing player data.
  */
-export class LocalDatabaseService {
+class LocalDatabaseService {
   private logger = getLogger('services/localdb.service')
   private DB: Low<{ players: LocalPlayerDAO[] }>
 
@@ -28,24 +28,25 @@ export class LocalDatabaseService {
    * @param player the player to save
    */
   savePlayer(player: Player) {
-    logger.log(`Saving player ${player.username} to DB...`)
+    this.logger.log(`Saving player ${player.username} to DB...`)
     const playerDAO = player.asLDAO()
-    DB.data.players.push(playerDAO)
+    this.DB.data.players.push(playerDAO)
   }
 
   /**
    * Get a player by name from local database.
    * @param playerName the name of the player to fetch
+   * @throws ItemNotFoundError if the player is not found in the database
    */
   getPlayerByName(playerName: string): Player {
-    logger.log(`Getting player ${playerName} from DB...`)
-    const {players} = DB.data
+    this.logger.log(`Getting player ${playerName} from DB...`)
+    const {players} = this.DB.data
 
     // Query the database for the player by username
     const foundPlayer = players.find((pDAO) => pDAO.username === playerName)
 
     if (!foundPlayer) {
-      logger.log(`Player ${playerName} not found in DB`)
+      this.logger.log(`Player ${playerName} not found in DB`)
       throw new ItemNotFoundError(`with name ${playerName}`)
     }
 
@@ -55,19 +56,22 @@ export class LocalDatabaseService {
   /**
    * Get a player by UUID from local database.
    * @param uuid the UUID of the player to fetch
+   * @throws ItemNotFoundError if the player is not found in the database
    */
   getPlayerByUuid(uuid: string): Player {
-    logger.info(`Getting player with UUID ${uuid} from DB...`)
-    const {players} = DB.data
+    this.logger.info(`Getting player with UUID ${uuid} from DB...`)
+    const {players} = this.DB.data
 
     // Query the database for the player by UUID
     const foundPlayer = players.find((pDAO) => pDAO.uuid === uuid)
 
     if (!foundPlayer) {
-      logger.log(`Player with UUID ${uuid} not found in DB`)
+      this.logger.log(`Player with UUID ${uuid} not found in DB`)
       throw new ItemNotFoundError(`with UUID ${uuid}`)
     }
 
     return foundPlayer.asPlayer()
   }
 }
+
+export const ldbInstance = new LocalDatabaseService()
