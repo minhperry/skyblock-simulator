db96e5e61773db512acaef855ddc1666b1f7becf
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-18 13:47:46 +0200

    use blue preset and add query client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,6 +13,27 @@ import {MatTableModule} from "@angular/material/table";
 import {NgOptimizedImage} from "@angular/common";
 import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import {provideTanStackQuery, QueryClient} from '@tanstack/angular-query-experimental';
+import {definePreset} from '@primeng/themes';
+
+const PrimeNgPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{sky.50}',
+      100: '{sky.100}',
+      200: '{sky.200}',
+      300: '{sky.300}',
+      400: '{sky.400}',
+      500: '{sky.500}',
+      600: '{sky.600}',
+      700: '{sky.700}',
+      800: '{sky.800}',
+      900: '{sky.900}',
+      950: '{sky.950}',
+    }
+  }
+})
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -33,9 +54,11 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch()),
     providePrimeNG({
       theme: {
-        preset: Aura,
-        options: {}
+        preset: PrimeNgPreset
       }
-    })
+    }),
+    provideTanStackQuery(
+      new QueryClient()
+    )
   ]
 };
