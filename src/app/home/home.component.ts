3504b534524f9b6c62b2f92231b403c9c9c1e20d
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-19 00:49:52 +0200

    why the fuck does neither query nor resource work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
-import {MarkdownComponent} from "ngx-markdown";
+import {Component, inject, resource} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {injectQuery} from '@tanstack/angular-query-experimental';
+import {catchError, firstValueFrom, lastValueFrom, throwError} from 'rxjs';
+import {Tab, TabList, TabPanel, TabPanels, Tabs} from 'primeng/tabs';
+
+interface ChangelogEntry {
+  component: string;
+  description: string;
+}
+
+interface ChangelogPatch {
+  version: string;
+  entries: ChangelogEntry[];
+}
+
+interface ChangelogMinorVersion {
+  version: string;
+  patches: ChangelogPatch[];
+}
 
 @Component({
   selector: 'sb-home',
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss',
-    imports: [MarkdownComponent]
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  imports: [TabPanel, Tabs, TabList, Tab, TabPanel, TabPanels]
 })
 export class HomeComponent {
-  ngxMarkdownVersion = '18.0.0'
+  changelog: ChangelogMinorVersion[] = [];
+
+  private http = inject(HttpClient);
+
+  changelogRes = resource({
+    loader: () => this.httpGet
+  })
+
+  private httpGet = firstValueFrom(this.http.get<ChangelogMinorVersion[]>('/changelog.json'))
+
+  changelogQuery = injectQuery(() => ({
+    queryKey: ['changelog'],
+    queryFn: () => firstValueFrom(
+      this.http.get<ChangelogMinorVersion[]>('/changelog.json').pipe(
+        catchError(err => {
+          return throwError(() => err)
+        })
+      )), // or lastValueFrom if you want to wait for the last value
+  }))
 }
