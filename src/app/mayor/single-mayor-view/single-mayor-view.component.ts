0169cedc1bb8dad4db76df173c2141a2fa07c933
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-23 23:44:01 +0200

    split each mayor to a single view

---
+++ src/app/mayor/single-mayor-view/single-mayor-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-mayor-view',
+  imports: [],
+  templateUrl: './single-mayor-view.component.html',
+  styleUrl: './single-mayor-view.component.scss'
+})
+export class SingleMayorViewComponent {
+
+}
