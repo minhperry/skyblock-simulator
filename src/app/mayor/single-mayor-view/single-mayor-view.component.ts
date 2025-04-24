e318a4f0e15a59ff40d4134499a75e50beb757c3
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-25 00:34:34 +0200

    finalize view of each single mayor

--- src/app/mayor/single-mayor-view/single-mayor-view.component.ts
+++ src/app/mayor/single-mayor-view/single-mayor-view.component.ts
@@ -3,27 +3,35 @@ import {
   HostListener,
   input,
   signal,
-  Inject
+  Inject, inject
 } from '@angular/core';
-import {Mayor} from '../../../interfaces/jerry';
-import {DOCUMENT, NgStyle} from '@angular/common';
+import {HOUR, MayorData, MayorTime, MINUTE} from '../../../interfaces/jerry';
+import {DOCUMENT, NgClass, NgStyle} from '@angular/common';
+import {MayorCycleComponent} from '../mayor-cycle/mayor-cycle.component';
+
+type TimeStringTuple = [MayorTime, string]
 
 @Component({
   selector: 'sb-single-mayor-view',
   imports: [
-    NgStyle
+    NgStyle,
+    NgClass
   ],
   templateUrl: './single-mayor-view.component.html',
   styleUrl: './single-mayor-view.component.scss'
 })
 export class SingleMayorViewComponent {
   // Inputs
-  mayor = input.required<Mayor>()
+  mayor = input.required<MayorData>()
   isActive = input<boolean>(false)
+  showSecond = input<boolean>(false)
 
   // Internal states
   isBeingHovered = signal(false)
   tooltipPos = {x: 0, y: 0}
+
+  #parent = inject(MayorCycleComponent)
+  now = this.#parent.currentTime
 
   // DOM objects
   #window: Window
@@ -34,18 +42,26 @@ export class SingleMayorViewComponent {
     this.screenWidth = this.#window.innerWidth
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = this.#window.innerWidth
+  }
 
+  @HostListener('touchstart', ['$event'])
   @HostListener('mouseenter', ['$event'])
   onMouseEnter(event: MouseEvent) {
     this.isBeingHovered.set(true)
     this.initializeLocation(event)
   }
 
+  @HostListener('click', ['$event'])
   @HostListener('mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
     this.initializeLocation(event)
   }
 
+  @HostListener('touchend')
+  @HostListener('touchcancel')
   @HostListener('mouseleave')
   onMouseLeave() {
     this.isBeingHovered.set(false)
@@ -60,6 +76,7 @@ export class SingleMayorViewComponent {
     return this.tooltipPos.x
   }
 
+  // For the hover tooltip
   initializeLocation(event: MouseEvent) {
     const screenWidth = this.#window.innerWidth
     const cursorX = event.pageX
@@ -79,4 +96,47 @@ export class SingleMayorViewComponent {
       };
     }
   }
+
+  formatLocalTime(unixTime: number): string {
+    return new Date(unixTime * 1000).toLocaleString([], {
+      hour: '2-digit', minute: '2-digit', second: this.showSecond() ? '2-digit' : undefined,
+      day: 'numeric', year: 'numeric', month: 'numeric'
+    });
+  }
+
+  hasEvent(mayor: MayorData): boolean {
+    return mayor.event !== undefined;
+  }
+
+  whenIsMayorComparedToNow(mayor: MayorData): TimeStringTuple {
+    const diffStart = mayor.startTime - this.now()
+    const diffEnd = mayor.endTime - this.now()
+
+    if (diffStart > 0) {
+      return ['future', this.formatTimeDifference(diffStart)]
+    } else if (diffEnd > 0) {
+      return ['present', this.formatTimeDifference(diffEnd)]
+    } else {
+      return ['past', this.formatTimeDifference(-diffEnd)]
+    }
+  }
+
+  private formatTimeDifference(timeInSec: number, formatDays = true): string {
+    const days = Math.floor(timeInSec / (HOUR * 24));
+    const hoursWithoutDay = Math.floor(timeInSec / HOUR);
+    const hoursWithDay = hoursWithoutDay % 24;
+    const minutes = Math.floor((timeInSec % HOUR) / MINUTE);
+    const seconds = Math.floor((timeInSec % HOUR) % MINUTE);
+
+    let returnString = ''
+
+    if (formatDays) returnString += `${days}d ${hoursWithDay}h ${minutes}m`
+    else returnString += `${hoursWithoutDay}h ${minutes}m`
+
+    if (this.showSecond()) returnString += ` ${seconds}s`
+
+    return returnString
+  }
+
+  protected readonly Date = Date;
 }
