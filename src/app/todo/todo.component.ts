126bed0a2d00ec772a9ff60dd0fe184beab639ac
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-05-22 20:50:19 +0200

    delete todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -3,8 +3,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'sb-todo',
-    templateUrl: './todo.component.html',
-    styleUrl: './todo.component.scss'
+    templateUrl: './todo.component.html'
 })
 export class TodoComponent {
 
