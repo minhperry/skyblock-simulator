bcfea5e8a45a44aa025af34c3d1583bcefc8c1a8
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-05-22 22:29:03 +0200

    remove old code

--- src/app/hotm/hotm.component.ts
+++ src/app/hotm/hotm.component.ts
@@ -3,7 +3,7 @@ import {Position} from './hotmData';
 import {Nullable} from '../../interfaces/types';
 import {NgClass} from '@angular/common';
 import {HotmService} from './hotm.service';
-import {StaticComponent} from './static.component';
+import {CardComponent} from './card.component';
 
 @Component({
   selector: 'sb-hotm',
@@ -11,7 +11,7 @@ import {StaticComponent} from './static.component';
   styleUrl: './hotm.component.scss',
   imports: [
     NgClass,
-    StaticComponent
+    CardComponent
   ]
 })
 export class HotmComponent {
@@ -22,45 +22,5 @@ export class HotmComponent {
 
   onCellClick(x: number, y: number) {
     this.selectedPos.set({x, y});
-    console.log(this.hotmServ.getOpenIds())
   }
-
-  // Processors
-
-
-  /*
-
-  // Processors
-
-  /*
-  protected modifySelectedLevel_(amount: number) {
-    if (!this.selected) return;
-
-    const selected = this.selected
-    const curr = selected.state.currentLevel as number;
-    const max = selected.perk.maxLevel as number;
-
-    selected.state.currentLevel = Math.min(max, Math.max(1, curr + amount));
-    const {x, y} = selected.position;
-    this.grid[y][x] = selected;
-    // console.log(this.grid[y][x].state.currentLevel)
-  }**
-
-  modifySelectedLevel(amount: number) {
-    const selected = this.sigSelected();
-    if (!selected) return;
-
-    const updatedNode = {
-      ...selected,
-      state: {
-        ...selected.state,
-        currentLevel: Math.min(
-          selected.perk.maxLevel as number,
-          Math.max(1, (selected.state.currentLevel || 0) + amount)
-        ),
-      },
-    };
-
-    this.hotmSS.updateNode(updatedNode);
-  }*/
 }
