b1bb8d3909e6f32255c75b40cef80ed53b343991
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-05-24 01:16:42 +0200

    add popup dialog for fetching name

--- src/app/hotm/hotm.component.ts
+++ src/app/hotm/hotm.component.ts
@@ -9,6 +9,13 @@ import {ParseMCPipe} from '../../pipes/parse-mc.pipe';
 import {SafeHtmlPipe} from 'primeng/menu';
 import {Button} from 'primeng/button';
 import {DialogService} from 'primeng/dynamicdialog';
+import {Dialog} from 'primeng/dialog';
+import {InputText} from 'primeng/inputtext';
+import {Message} from 'primeng/message';
+import {Select} from 'primeng/select';
+import {FormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment.development';
 
 @Component({
   selector: 'sb-hotm',
@@ -20,7 +27,12 @@ import {DialogService} from 'primeng/dynamicdialog';
     ColorizePipe,
     ParseMCPipe,
     SafeHtmlPipe,
-    Button
+    Button,
+    Dialog,
+    InputText,
+    Message,
+    Select,
+    FormsModule
   ],
   providers: [DialogService]
 })
@@ -28,7 +40,7 @@ export class HotmComponent {
   hotmServ = inject(HotmService);
   selectedPos: WritableSignal<Nullable<Position>> = signal(null)
 
-  dialogServ = inject(DialogService)
+  dialogVisible = false
 
   // Click handlers
 
@@ -36,6 +48,68 @@ export class HotmComponent {
     this.selectedPos.set({x, y});
   }
 
+  openDialog() {
+    this.dialogVisible = true
+  }
+
+  // Profile stuffs
+  ign = ''
+  profiles: Profile[] = []
+  selectedProfile = this.profiles[0];
+
+  selectDisabled = true
+
+  http = inject(HttpClient)
+  backend = environment.backendUrl
+
+  submitUsername() {
+    //this.profiles = []
+    this.http.get<ProfileResponse[]>(this.backend + '/profiles/' + this.ign).subscribe({
+      next: (val: ProfileResponse[]) => {
+        for (const prof of val) {
+          const profile = responseToProfileMapper(prof)
+          this.profiles.push(profile)
+        }
+        this.selectDisabled = false
+        console.log(this.profiles)
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
+  }
+
   protected readonly formattedPowderString = formattedPowderString;
   protected readonly PowderType = PowderType;
+}
+
+function responseToProfileMapper(prof: ProfileResponse): Profile {
+  let icon = '';
+  switch (prof.gameMode) {
+    case 'ironman':
+      icon = '♻'
+      break;
+    case 'bingo':
+      icon = 'Ⓑ' // letter B in circle
+      break;
+    case 'island':
+      icon = '☀'
+      break;
+  }
+  return {
+    displayName: `${prof.active ? '>>>' : ''} ${icon} ${prof.profileFruit}`,
+    uuid: prof.profileId
+  }
+}
+
+interface Profile {
+  displayName: string;
+  uuid: string;
+}
+
+interface ProfileResponse {
+  active: boolean;
+  gameMode: 'normal' | 'ironman' | 'bingo' | 'island';
+  profileFruit: string;
+  profileId: string;
 }
