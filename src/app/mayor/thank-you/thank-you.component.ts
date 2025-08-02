680063bcb3de6fd2e33d8dfc2de00e8762925b1d
Tuan Minh Phan <me@minhperry.de>
2025-08-02 14:14:00 +0200

    update next jerry

--- src/app/mayor/thank-you/thank-you.component.ts
+++ src/app/mayor/thank-you/thank-you.component.ts
@@ -3,50 +3,52 @@ import {DAY, HOUR, MINUTE} from "../../../interfaces/jerry";
 import {NullableInterval, Utils} from "../../../services/utils"
 
 @Component({
-    selector: 'sb-thank-you',
-    templateUrl: './thank-you.component.html',
-    styleUrl: './thank-you.component.scss'
+  selector: "sb-thank-you",
+  templateUrl: "./thank-you.component.html",
+  styleUrl: "./thank-you.component.scss",
 })
 export class ThankYouComponent implements OnInit, OnDestroy {
-    nextJerry = 1749654900
-    current = 0
-    private interval: NullableInterval = null;
+  nextJerry = 1760368500;
+  current = 0;
+  private interval: NullableInterval = null;
 
-    constructor(@Inject(PLATFORM_ID) private platform: object) {
-    }
+  constructor(@Inject(PLATFORM_ID) private platform: object) {}
 
-    calcDiff() {
-        let diff = - this.current + this.nextJerry
-        const day = Math.floor(diff / DAY)
-        diff %= DAY
-        const hour = Math.floor(diff / HOUR)
-        diff %= HOUR
-        const minute = Math.floor(diff / MINUTE)
-        const second = diff % MINUTE
-        return `${day}d ${hour}h ${minute}m ${second}s`
-    }
+  calcDiff() {
+    let diff = -this.current + this.nextJerry;
+    const day = Math.floor(diff / DAY);
+    diff %= DAY;
+    const hour = Math.floor(diff / HOUR);
+    diff %= HOUR;
+    const minute = Math.floor(diff / MINUTE);
+    const second = diff % MINUTE;
+    return `${day}d ${hour}h ${minute}m ${second}s`;
+  }
 
-    formatNextJerry() {
-        return new Date(this.nextJerry * 1000).toLocaleString([], {
-            hour: '2-digit', minute: '2-digit',
-            day: 'numeric', year: 'numeric', month: "numeric"
-        });
-    }
+  formatNextJerry() {
+    return new Date(this.nextJerry * 1000).toLocaleString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      day: "numeric",
+      year: "numeric",
+      month: "numeric",
+    });
+  }
 
-    ngOnInit() {
-        this.updateTime()
-        Utils.doIfBrowser(this.platform, () => {
-            this.interval = setInterval(() => this.updateTime(), 1000)
-        })
-    }
+  ngOnInit() {
+    this.updateTime();
+    Utils.doIfBrowser(this.platform, () => {
+      this.interval = setInterval(() => this.updateTime(), 1000);
+    });
+  }
 
-    ngOnDestroy(): void {
-        if (this.interval) {
-            clearInterval(this.interval)
-        }
+  ngOnDestroy(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
     }
+  }
 
-    private updateTime(): void {
-        this.current = Math.floor(Date.now() / 1000);
-    }
+  private updateTime(): void {
+    this.current = Math.floor(Date.now() / 1000);
+  }
 }
